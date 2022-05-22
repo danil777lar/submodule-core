f35e4005c8d94dfa342822b8812a3d0325054187
daniil.larzhevskiy <daniil.larzhevskiy@playcus.com>
2022-05-22 03:55:24 +0300

    trajectory dawer

--- Utils/Trajectory Drawer/Source/TrajectoryDrawer.cs
+++ Utils/Trajectory Drawer/Source/TrajectoryDrawer.cs
@@ -11,6 +11,9 @@ public class TrajectoryDrawer : MonoBehaviour
     [SerializeField] private string[] _ignoreLayers;
     [Header("Visual")]
     [SerializeField] private float _startCutLenght;
+    [SerializeField] private float _totalLenght;
+    [SerializeField] private Transform _drawRoot;
+    [Space]
     [SerializeField] private LineRenderer _linePrefab;
     [SerializeField] private ParticleSystem _partsPrefab;
     [SerializeField] private ParticleSystem _hitPrefab;
@@ -28,7 +31,7 @@ public class TrajectoryDrawer : MonoBehaviour
 
         float cuttedLenght = 0f;
         Vector3 velocity = force;
-        Vector3 position = transform.position;
+        Vector3 position = _drawRoot ? _drawRoot.position : transform.position;
         List<Vector3> points = new List<Vector3>();
         for (int i = 0; i < 100; i++)
         {
@@ -49,8 +52,26 @@ public class TrajectoryDrawer : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(prevPosition, position - prevPosition, velocity.magnitude * _updateDelta, ~LayerMask.GetMask(_ignoreLayers));
             if (hit)
             {
-                points.Add(hit.point);
+                points.Add(new Vector3(hit.point.x, hit.point.y, prevPosition.z));
                 break;
+            }
+        }
+
+        float totalLenght = 0f;
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            if (totalLenght + Vector3.Distance(points[i], points[i + 1]) >= _totalLenght)
+            {
+                points[i + 1] = points[i] + (points[i + 1] - points[i]).normalized * (_totalLenght - totalLenght);
+                if (i < points.Count - 2)
+                {
+                    points.RemoveRange(i + 2, points.Count - (i + 2));
+                    break;
+                }
+            }
+            else 
+            {
+                totalLenght += Vector3.Distance(points[i], points[i + 1]);
             }
         }
 
