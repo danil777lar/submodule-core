251035cd1c1ec6c8dd50b39765c7b57f27867c99
Larjevskiy Daniil <larzhevskiy.d@itcomp.org>
2022-09-03 22:37:10 +0300

    update ui and utility

---
+++ Services/UI/Elements/RectTransformEvents.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Larje.Core.Services.UI
+{
+    public class RectTransformEvents : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        public Action<PointerEventData> PointerDown;
+        public Action<PointerEventData> PointerMove;
+        public Action<PointerEventData> PointerUp;
+        public Action<PointerEventData> PointerEnter;
+        public Action<PointerEventData> PointerExit;
+
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            PointerDown?.Invoke(eventData);
+        }
+
+        public void OnPointerMove(PointerEventData eventData)
+        {
+            PointerMove?.Invoke(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            PointerUp?.Invoke(eventData);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PointerEnter?.Invoke(eventData);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            PointerExit?.Invoke(eventData);
+        }
+    }
+}
--- Utils/LarjeUtility.cs
+++ Utils/LarjeUtility.cs
@@ -11,5 +11,24 @@ namespace Larje.Core.Utility
             RaycastHit2D[] results = new RaycastHit2D[1];
             return collider.Cast(direction, results, distance) > 0;
         }
+
+        public static Vector2Int FloorVector2(Vector2 vector) 
+        {
+            Vector2Int result = Vector2Int.zero;
+            result.x = Mathf.FloorToInt(vector.x);
+            result.y = Mathf.FloorToInt(vector.y);
+            return result;
+        }
+
+        public static Color SetColorAlpha(Color color, float alphaValue) 
+        {
+            return new Color(color.a, color.g, color.b, alphaValue);
+        }
+
+        public static Color SetColorSaturation(Color color, float saturationValue) 
+        {
+            float grayValue = (color.a + color.g + color.b) / 3f;
+            return Color.LerpUnclamped(Color.white * grayValue, color, saturationValue);
+        }
     }
 }
