880b621fdc47660ac60bd05cf48e66c55b66992b
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-04-13 17:09:14 +0300

    character seek complete!!!

--- Tools/TopDownEngine Extensions/Abilities/CoreCharacterOrientation3D.cs
+++ Tools/TopDownEngine Extensions/Abilities/CoreCharacterOrientation3D.cs
@@ -11,22 +11,23 @@ namespace Larje.Core.Tools.TopDownEngine
         public Transform forceLookTarget;
         
         [SerializeField] private Vector3 rotationMultiplier = Vector3.one;
-        [Space] 
-        [SerializeField] private float lookAngle;
-        [Space]
+        [Header("Main")]
         [SerializeField] private float minRotationSpeed;
         [SerializeField] private float maxRotationSpeed;
-        [Space] 
+        [Header("Look")] 
+        [SerializeField, Range(0f, 360f)] private float lookAngle;
+        [SerializeField] private float lookSpeed;
         [SerializeField] private Transform modelLook;
+        
 
-        private float _lastRotateTime;
+        private float _lastUpdateTime;
         private Vector3 _lastPosition;
         private Vector3 _currentDirection;
         private Vector3 _currentLookDirection;
         private CoreCharacterMovement _coreMovement;
         
         public Vector3 Direction => _currentDirection;
-        public Vector3 LookDirection => _currentLookDirection;
+        public Vector3 LookDirection => modelLook.forward;
 
         public override void ProcessAbility()
         {
@@ -42,12 +43,16 @@ namespace Larje.Core.Tools.TopDownEngine
                 return;
             }
 
+            float deltaTime = Time.time - _lastUpdateTime;
+
+            CatchLookDirection();
+            RotateLook(deltaTime);
+            
             CatchDirection();
             ApplyLimit();
-            Rotate();
-            
-            CatchLookDirection();
-            RotateLook();
+            Rotate(deltaTime);
+
+            _lastUpdateTime = Time.time;
         }
 
         protected override void Initialization()
@@ -88,43 +93,40 @@ namespace Larje.Core.Tools.TopDownEngine
 
         private void ApplyLimit()
         {
-            if (forceLookTarget)
-            {
-                Vector3 limitDirection = transform.position - forceLookTarget.position;
-                
-                float angle = Vector3.Angle(limitDirection, _currentDirection);
-                float signedAngle = Vector3.SignedAngle(limitDirection, _currentDirection, Vector3.up);
+            Vector3 limitDirection = -LookDirection;
 
-                if (angle < lookAngle * 0.5f)
-                {
-                    float rotateAngle = lookAngle * 0.5f - angle;
-                    rotateAngle *= signedAngle < 0f ? -1 : 1f;
-                    _currentDirection = Quaternion.Euler(0, rotateAngle, 0) * _currentDirection;
-                }   
+            float limit = 360f - lookAngle; 
+            float angle = Vector3.Angle(limitDirection, _currentDirection);
+            float signedAngle = Vector3.SignedAngle(limitDirection, _currentDirection, Vector3.up);
+
+            if (angle < limit * 0.5f)
+            {
+                float rotateAngle = limit * 0.5f - angle;
+                rotateAngle *= signedAngle < 0f ? -1 : 1f;
+                _currentDirection = Quaternion.Euler(0, rotateAngle, 0) * _currentDirection;
             }
         }
         
-        private void Rotate()
+        private void Rotate(float deltaTime)
         {
-            float timeDelta = Time.time - _lastRotateTime; 
             if (_currentDirection != Vector3.zero)
             {
                 float rotationSpeed = Mathf.Lerp(minRotationSpeed, maxRotationSpeed,
-                    _coreMovement.ActualSpeedPercent) * timeDelta;
+                    _coreMovement.ActualSpeedPercent) * deltaTime;
                 Quaternion rotation = Quaternion.LookRotation(_currentDirection);
                 rotation = Quaternion.Euler(Vector3.Scale(rotation.eulerAngles, rotationMultiplier));
                 _character.CharacterModel.transform.rotation = Quaternion.RotateTowards(
                     _character.CharacterModel.transform.rotation, rotation, rotationSpeed);
             }
-            _lastRotateTime = Time.time;
         }
 
-        private void RotateLook()
+        private void RotateLook(float deltaTime)
         {
             if (modelLook != null && _currentLookDirection != Vector3.zero)
             {
                 Quaternion rotation = Quaternion.LookRotation(_currentLookDirection);
-                modelLook.rotation = Quaternion.Lerp(modelLook.rotation, rotation, Time.fixedDeltaTime * 10f);
+                modelLook.rotation = Quaternion.RotateTowards(modelLook.rotation, rotation, 
+                    lookSpeed * deltaTime);
             }
         }
     }
