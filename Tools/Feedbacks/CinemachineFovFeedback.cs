ba719f20e0f8622cff8774ec74155e83e8adf223
Daniil Larzhevskii <danil777lar@gmail.com>
2024-06-03 01:27:41 +0400

    add cinemachine camera fov feedback

---
+++ Tools/Feedbacks/CinemachineFovFeedback.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+[FeedbackPath("Camera/Cinemachine Fov")]
+public class CinemachineFovFeedback : MMF_FeedbackBase
+{
+    #if UNITY_EDITOR
+    public override Color FeedbackColor { get { return MMFeedbacksInspectorColors.CameraColor; } }
+    #endif
+    
+    [Space] 
+    public int ListenerChannel;
+    public AnimationCurve CurveIntensity;
+ 
+    protected override void FillTargets()
+    {
+        
+    }
+
+    public override void Play(Vector3 position, float feedbacksIntensity = 1)
+    {
+        base.Play(position, feedbacksIntensity);
+        CinemachineFovListener.SetFovModifier(ListenerChannel, CurveIntensity.Evaluate(feedbacksIntensity));
+    }
+
+    public override void Stop(Vector3 position, float feedbacksIntensity = 1)
+    {
+        base.Stop(position, feedbacksIntensity);
+        CinemachineFovListener.SetFovModifier(ListenerChannel, 1f);
+    }
+
+    public override void ResetFeedback()
+    {
+        base.ResetFeedback();
+        CinemachineFovListener.SetFovModifier(ListenerChannel, 1f);
+    }
+}
---
+++ Tools/Feedbacks/CinemachineFovListener.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CinemachineFovListener : MonoBehaviour
+{
+    private static Dictionary<int, List<CinemachineFovListener>> _listeners;
+ 
+    public static void SetFovModifier(int channel, float modifier)
+    {
+        foreach (var listener in GetListenersList(channel))
+        {
+            listener.SetModifier(modifier);
+        }
+    }
+    
+    private static List<CinemachineFovListener> GetListenersList(int channel)
+    {
+        _listeners ??= new Dictionary<int, List<CinemachineFovListener>>();
+
+        if (!_listeners.ContainsKey(channel))
+        {
+            _listeners[channel] = new List<CinemachineFovListener>();
+        }
+
+        return _listeners[channel];
+    } 
+    
+    [SerializeField] private int channel;
+    [SerializeField] private float fadeSharpness;
+
+    private float _targetModifier;
+    private float _defaultFov;
+    private CinemachineVirtualCamera _virtualCamera;
+
+    private void Awake()
+    {
+        _targetModifier = 1f;
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        _defaultFov = _virtualCamera.m_Lens.FieldOfView;
+        
+        GetListenersList(channel).Add(this);
+    }
+
+    private void Update()
+    {
+        _virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(_virtualCamera.m_Lens.FieldOfView, _defaultFov * _targetModifier, 
+            Time.deltaTime * fadeSharpness);
+    }
+
+    private void OnDestroy()
+    {
+        GetListenersList(channel).Remove(this);
+    }
+
+    private void SetModifier(float modifier)
+    {
+        _targetModifier = modifier;
+    }
+}
