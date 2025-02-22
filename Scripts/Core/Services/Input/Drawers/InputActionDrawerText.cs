4110c095d0bd55dc2e415018ba9003836ecca92d
danil777lar <danil777lar@gmail.com>
2025-02-23 01:28:37 +0400

    add input action draw

---
+++ Scripts/Core/Services/Input/Drawers/InputActionDrawerText.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Input Action Drawer Text", menuName = "Larje/Core/Services/Input/Drawers/Text", order = 1)]
+public class InputActionDrawerText : InputActionDrawer
+{
+    [SerializeField] private string leftModifier;
+    [SerializeField] private string rightModifier;
+    
+    public override object Draw(InputAction action)
+    {
+        string title = "";
+
+        if (action.type == InputActionType.Button)
+        {
+            title = action.bindings.First().path;
+        }
+
+        return $"{leftModifier}{title}{rightModifier}";
+    }
+}
