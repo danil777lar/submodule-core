08da4c3f350abdab1e4beaccf7886b6502a83e67
danil777lar <danil777lar@gmail.com>
2025-04-04 19:17:06 +0400

    add block control option on jump

--- Scripts/Tools/TopDownEngine Extensions/Abilities/CoreCharacterJump3D.cs
+++ Scripts/Tools/TopDownEngine Extensions/Abilities/CoreCharacterJump3D.cs
@@ -5,35 +5,39 @@ using MoreMountains.Feedbacks;
 using MoreMountains.Tools;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Larje.Core.Tools.TopDownEngine
 {
 	public class CoreCharacterJump3D : CharacterAbility
 	{
 		[Header("Jump Settings")] 
-		public bool JumpProportionalToPress = true;
-		public float MinimumPressTime = 0.4f;
-		public float JumpForce = 800f;
-		public float JumpHeight = 4f;
-		public AnimationCurve JumpForceCurve = AnimationCurve.Constant(0f, 1f, 1f);
-
+		[SerializeField] private bool jumpProportionalToPress = true;
+		[SerializeField] private bool blockControlOnJump;
+		[SerializeField] private float minimumPressTime = 0.4f;
+		[SerializeField] private float jumpForce = 800f;
+		[SerializeField] private float jumpHeight = 4f;
+		[SerializeField] private AnimationCurve jumpForceCurve = AnimationCurve.Constant(0f, 1f, 1f);
+		
 		[Header("Slopes")] 
-		public bool CanJumpOnTooSteepSlopes = true;
-		public bool ResetJumpsOnTooSteepSlopes = false;
-
+		[SerializeField] private bool canJumpOnTooSteepSlopes = true;
+		[SerializeField] private bool resetJumpsOnTooSteepSlopes = false;
+		
 		[Header("Number of Jumps")] 
-		public int NumberOfJumps = 1;
-		[MMReadOnly] public int NumberOfJumpsLeft = 0;
-
+		[SerializeField] private int numberOfJumps = 1;
+		[SerializeField, MMReadOnly] public int numberOfJumpsLeft = 0;
+		
 		[Header("Feedbacks")] 
-		public MMFeedbacks JumpStartFeedback;
-		public MMFeedbacks JumpStopFeedback;
+		[SerializeField] private MMFeedbacks jumpStartFeedback;
+		[SerializeField] private MMFeedbacks jumpStopFeedback;
 		
 		protected Vector3 _jumpForce;
 		protected Vector3 _jumpOrigin;
-		protected CharacterButtonActivation _characterButtonActivation;
 		protected CharacterCrouch _characterCrouch;
+		protected CoreCharacterMovement _characterMove;
+		protected CharacterButtonActivation _characterButtonActivation;
 		protected bool _doubleJumping;
+		protected bool _isInJump;
 		protected bool _jumpStopped = false;
 		protected bool _buttonReleased = false;
 		protected int _initialNumberOfJumps;
@@ -45,6 +49,8 @@ namespace Larje.Core.Tools.TopDownEngine
 		protected int _jumpingAnimationParameter;
 		protected int _doubleJumpingAnimationParameter;
 		protected int _hitTheGroundAnimationParameter;
+		
+		public bool IsInJump => _isInJump;
 		
 		public event Action EventJump;
 		public event Action EventLanding;
@@ -65,104 +71,6 @@ namespace Larje.Core.Tools.TopDownEngine
 			}
 		}
 		
-		protected override void Initialization()
-		{
-			base.Initialization();
-
-			ResetNumberOfJumps();
-			_jumpStopped = true;
-			_characterButtonActivation = _character?.FindAbility<CharacterButtonActivation>();
-			_characterCrouch = _character?.FindAbility<CharacterCrouch>();
-			JumpStartFeedback?.Initialization(this.gameObject);
-			JumpStopFeedback?.Initialization(this.gameObject);
-			_initialNumberOfJumps = NumberOfJumps;
-		}
-
-		public override void ProcessAbility()
-		{
-			if (_controller.JustGotGrounded)
-			{
-				ResetNumberOfJumps();
-			}
-
-			if (!AbilityAuthorized
-			    || (_condition.CurrentState != CharacterStates.CharacterConditions.Normal))
-			{
-				return;
-			}
-
-			CheckMovementStates();
-			TryUpdateJump();
-			TryResetNumberOfJumps();
-
-			if (_controller.JustGotGrounded)
-			{
-				EventLanding?.Invoke();
-			}
-		}
-
-		protected virtual void CheckMovementStates()
-		{
-			if (!_jumpStopped
-			    &&
-			    ((_movement.CurrentState == CharacterStates.MovementStates.Idle)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Walking)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Running)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Crouching)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Crawling)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Pushing)
-			     || (_movement.CurrentState == CharacterStates.MovementStates.Falling)
-			    ))
-			{
-				JumpStop();
-			}
-		}
-
-		protected virtual void TryUpdateJump()
-		{
-			if (_movement.CurrentState == CharacterStates.MovementStates.Jumping)
-			{
-				if (_buttonReleased && !_jumpStopped && JumpProportionalToPress && (Time.time - _jumpStartedAt > MinimumPressTime))
-				{
-					JumpStop();
-				}
-
-				if (!_jumpStopped)
-				{
-					if ((this.transform.position.y - _jumpOrigin.y > JumpHeight) || CeilingTest())
-					{
-						JumpStop();
-						_controller3D.Grounded = _controller3D.IsGroundedTest();
-						if (_controller.Grounded)
-						{
-							ResetNumberOfJumps();
-						}
-					}
-					else
-					{
-						float heightPercent = (transform.position.y - _jumpOrigin.y) / JumpHeight;
-						float force = JumpForceCurve.Evaluate(heightPercent) * JumpForce * Time.deltaTime;
-						_jumpForce = Vector3.up * force;
-						_controller.AddForce(_jumpForce);
-					}
-				}
-			}
-		}
-
-		protected virtual void TryResetNumberOfJumps()
-		{
-			if (!ResetJumpsOnTooSteepSlopes && _controller3D.ExitedTooSteepSlopeThisFrame && _controller3D.Grounded)
-			{
-				ResetNumberOfJumps();
-			}
-		}
-
-	protected virtual bool CeilingTest()
-		{
-			bool returnValue = _controller3D.CollidingAbove();
-			return returnValue;
-		}
-		
 		public virtual void JumpStart()
 		{
 			if (!EvaluateJumpConditions())
@@ -170,16 +78,17 @@ namespace Larje.Core.Tools.TopDownEngine
 				return;
 			}
 			
-			if (NumberOfJumpsLeft != NumberOfJumps)
+			if (numberOfJumpsLeft != numberOfJumps)
 			{
 				_doubleJumping = true;
 			}
 
-			NumberOfJumpsLeft = NumberOfJumpsLeft - 1;
+			numberOfJumpsLeft = numberOfJumpsLeft - 1;
 
+			_isInJump = true;
 			_movement.ChangeState(CharacterStates.MovementStates.Jumping);
 			MMCharacterEvent.Trigger(_character, MMCharacterEventTypes.Jump);
-			JumpStartFeedback?.PlayFeedbacks(this.transform.position);
+			jumpStartFeedback?.PlayFeedbacks(this.transform.position);
 			_jumpOrigin = this.transform.position;
 			_jumpStopped = false;
 			_jumpStartedAt = Time.time;
@@ -208,14 +117,15 @@ namespace Larje.Core.Tools.TopDownEngine
 			StopAbilityUsedSfx();
 			StopStartFeedbacks();
 			PlayAbilityStopFeedbacks();
-			JumpStopFeedback?.PlayFeedbacks(this.transform.position);
+			jumpStopFeedback?.PlayFeedbacks(this.transform.position);
 		}
 		
 		public virtual void ResetNumberOfJumps()
 		{
+			_isInJump = false;
 			bool shouldResetJumps = true;
 
-			if (!ResetJumpsOnTooSteepSlopes)
+			if (!resetJumpsOnTooSteepSlopes)
 			{
 				if (_controller3D.TooSteep())
 				{
@@ -225,10 +135,127 @@ namespace Larje.Core.Tools.TopDownEngine
 
 			if (shouldResetJumps)
 			{
-				NumberOfJumpsLeft = NumberOfJumps;
+				numberOfJumpsLeft = numberOfJumps;
 			}
 
 			_doubleJumping = false;
+		}
+		
+		public override void ProcessAbility()
+		{
+			if (_controller.JustGotGrounded)
+			{
+				ResetNumberOfJumps();
+			}
+
+			if (!AbilityAuthorized
+			    || (_condition.CurrentState != CharacterStates.CharacterConditions.Normal))
+			{
+				return;
+			}
+
+			CheckMovementStates();
+			TryUpdateJump();
+			TryResetNumberOfJumps();
+
+			if (_controller.JustGotGrounded)
+			{
+				EventLanding?.Invoke();
+			}
+		}
+		
+		public override void UpdateAnimator()
+		{
+			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _jumpingAnimationParameter,
+				(_movement.CurrentState == CharacterStates.MovementStates.Jumping), _character._animatorParameters,
+				_character.RunAnimatorSanityChecks);
+			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _doubleJumpingAnimationParameter, _doubleJumping,
+				_character._animatorParameters, _character.RunAnimatorSanityChecks);
+			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _hitTheGroundAnimationParameter,
+				_controller.JustGotGrounded, _character._animatorParameters, _character.RunAnimatorSanityChecks);
+		}
+		
+		protected override void Initialization()
+		{
+			base.Initialization();
+
+			ResetNumberOfJumps();
+			_jumpStopped = true;
+			
+			_characterButtonActivation = _character?.FindAbility<CharacterButtonActivation>();
+			_characterCrouch = _character?.FindAbility<CharacterCrouch>();
+			_characterMove = _character?.FindAbility<CoreCharacterMovement>();
+			
+			jumpStartFeedback?.Initialization(this.gameObject);
+			jumpStopFeedback?.Initialization(this.gameObject);
+			_initialNumberOfJumps = numberOfJumps;
+			
+			if (blockControlOnJump)
+			{
+				_characterMove.TryAddChangeDirectionCondition(() => !_isInJump);
+			}
+		}
+
+		protected virtual void CheckMovementStates()
+		{
+			if (!_jumpStopped
+			    &&
+			    ((_movement.CurrentState == CharacterStates.MovementStates.Idle)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Walking)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Running)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Crouching)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Crawling)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Pushing)
+			     || (_movement.CurrentState == CharacterStates.MovementStates.Falling)
+			    ))
+			{
+				JumpStop();
+			}
+		}
+
+		protected virtual void TryUpdateJump()
+		{
+			if (_movement.CurrentState == CharacterStates.MovementStates.Jumping)
+			{
+				if (_buttonReleased && !_jumpStopped && jumpProportionalToPress && (Time.time - _jumpStartedAt > minimumPressTime))
+				{
+					JumpStop();
+				}
+
+				if (!_jumpStopped)
+				{
+					if ((this.transform.position.y - _jumpOrigin.y > jumpHeight) || CeilingTest())
+					{
+						JumpStop();
+						_controller3D.Grounded = _controller3D.IsGroundedTest();
+						if (_controller.Grounded)
+						{
+							ResetNumberOfJumps();
+						}
+					}
+					else
+					{
+						float heightPercent = (transform.position.y - _jumpOrigin.y) / jumpHeight;
+						float force = jumpForceCurve.Evaluate(heightPercent) * jumpForce * Time.deltaTime;
+						_jumpForce = Vector3.up * force;
+						_controller.AddForce(_jumpForce);
+					}
+				}
+			}
+		}
+
+		protected virtual void TryResetNumberOfJumps()
+		{
+			if (!resetJumpsOnTooSteepSlopes && _controller3D.ExitedTooSteepSlopeThisFrame && _controller3D.Grounded)
+			{
+				ResetNumberOfJumps();
+			}
+		}
+
+		protected virtual bool CeilingTest()
+		{
+			bool returnValue = _controller3D.CollidingAbove();
+			return returnValue;
 		}
 		
 		protected virtual bool EvaluateJumpConditions()
@@ -248,7 +275,7 @@ namespace Larje.Core.Tools.TopDownEngine
 				}
 			}
 
-			if (!CanJumpOnTooSteepSlopes)
+			if (!canJumpOnTooSteepSlopes)
 			{
 				if (_controller3D.TooSteep())
 				{
@@ -269,7 +296,7 @@ namespace Larje.Core.Tools.TopDownEngine
 				return false;
 			}
 
-			if (NumberOfJumpsLeft <= 0)
+			if (numberOfJumpsLeft <= 0)
 			{
 				return false;
 			}
@@ -290,17 +317,6 @@ namespace Larje.Core.Tools.TopDownEngine
 				out _doubleJumpingAnimationParameter);
 			RegisterAnimatorParameter(_hitTheGroundAnimationParameterName, AnimatorControllerParameterType.Bool,
 				out _hitTheGroundAnimationParameter);
-		}
-		
-		public override void UpdateAnimator()
-		{
-			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _jumpingAnimationParameter,
-				(_movement.CurrentState == CharacterStates.MovementStates.Jumping), _character._animatorParameters,
-				_character.RunAnimatorSanityChecks);
-			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _doubleJumpingAnimationParameter, _doubleJumping,
-				_character._animatorParameters, _character.RunAnimatorSanityChecks);
-			MMAnimatorExtensions.UpdateAnimatorBool(_animator, _hitTheGroundAnimationParameter,
-				_controller.JustGotGrounded, _character._animatorParameters, _character.RunAnimatorSanityChecks);
 		}
 	}
 }
