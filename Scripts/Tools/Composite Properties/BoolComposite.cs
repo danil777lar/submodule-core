b180a2c3839547141512b403974778e874b3d111
danil777lar <danil777lar@gmail.com>
2025-04-10 17:12:08 +0400

    add floating

---
+++ Scripts/Tools/Composite Properties/BoolComposite.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Larje.Core.Tools.CompositeProperties
+{
+    [Serializable]
+    public class BoolComposite
+    {
+        [SerializeField] private bool defaultValue;
+        [SerializeField] private Operation operation;
+        
+        private List<Func<bool>> _values = new List<Func<bool>>();
+        
+        public bool Value
+        {
+            get
+            {
+                bool result = defaultValue;
+                foreach (Func<bool> value in _values)
+                {
+                    if (operation == Operation.And)
+                    {
+                        result &= value();
+                    }
+                    else if (operation == Operation.Or)
+                    {
+                        result |= value();
+                    }
+                }
+
+                return result;
+            }
+        }
+        
+        public void AddValue(Func<bool> value)
+        {
+            if (!_values.Contains(value))
+            {
+                _values.Add(value);
+            }
+        }
+
+        public void RemoveValue(Func<bool> value)
+        {
+            if (_values.Contains(value))
+            {
+                _values.Remove(value);
+            }
+        }
+        
+        public enum Operation
+        {
+            And,
+            Or
+        }
+    }
+}
