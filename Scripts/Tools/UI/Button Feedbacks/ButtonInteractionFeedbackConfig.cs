f4ba33296d6263fc6143686968832302e6cee67d
Daniil Larzhevskii <danil777lar@gmail.com>
2025-07-19 15:21:07 +0400

    save button config

--- Scripts/Tools/UI/Button Feedbacks/ButtonInteractionFeedbackConfig.cs
+++ Scripts/Tools/UI/Button Feedbacks/ButtonInteractionFeedbackConfig.cs
@@ -14,5 +14,12 @@ namespace Larje.Core.Tools
             
         public Material Material => material;
         public IReadOnlyCollection<ButtonInteractionState> States => states;
+
+        private void OnValidate()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(this);
+            #endif
+        }
     }
 }
