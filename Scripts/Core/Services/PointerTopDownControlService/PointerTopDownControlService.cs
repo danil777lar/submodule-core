27c65ebba2bd4022e436b20574326dc8410b1547
danya <danil777lar@gmail.com>
2025-10-13 14:35:43 -0300

    update cinemachine references

--- Scripts/Core/Services/PointerTopDownControlService/PointerTopDownControlService.cs
+++ Scripts/Core/Services/PointerTopDownControlService/PointerTopDownControlService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Cinemachine;
 using DG.Tweening;
 using Larje.Core;
 using Larje.Core.Services;
--- Scripts/Core/Services/Sound/Core/Sound.cs
+++ Scripts/Core/Services/Sound/Core/Sound.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Cinemachine;
 using Larje.Core.Services;
 using UnityEngine;
 using UnityEngine.Events;
