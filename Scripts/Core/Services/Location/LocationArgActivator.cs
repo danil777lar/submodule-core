eb2c170ff440cb4da3ec6c8c4981752cc7037758
Danil Kolbasenko <danil777lar@gmail.com>
2026-03-07 17:02:12 -0300

    update location arg activator

--- Scripts/Core/Services/Location/LocationArgActivator.cs
+++ Scripts/Core/Services/Location/LocationArgActivator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Larje.Core;
 using ProjectConstants;
@@ -5,10 +6,8 @@ using UnityEngine;
 
 public class LocationArgActivator : MonoBehaviour
 {
-    [SerializeField] private LocationArgType argType;
-    [SerializeField] private GameObject targetObject;
-    [Space]
-    [SerializeField] private TriggerConstant trigger;
+    [SerializeField] private bool permitted = true;
+    [SerializeField] private Option[] options;
     
     [InjectService] private LocationService _locationService;
     [InjectService] private GameEventService _gameEventService;
@@ -17,12 +16,40 @@ public class LocationArgActivator : MonoBehaviour
     {
         DIContainer.InjectTo(this);
 
-        bool isActive = _locationService.CurrentLocation.LocationArgs.Contains(argType);
-        targetObject?.SetActive(isActive);
-
-        if (isActive && trigger != null)
+        if (permitted)
         {
-            _gameEventService.SendEvent(new GameEventTrigger(trigger, 1f, gameObject.name));
+            foreach (Option option in options)
+            {
+                bool isActive = _locationService.CurrentLocation.LocationArgs.Contains(option.ArgType);
+                option.TargetObject.SetActive(isActive);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (options == null || options.Length == 0)
+        {
+            return;
+        }
+
+        foreach (Option option in options)
+        {
+            option.OnValidate();
+        }
+    }
+
+    [Serializable]
+    private class Option
+    {
+        [HideInInspector, SerializeField] public string inspectorName;
+
+        [field: SerializeField] public LocationArgType ArgType { get; private set; }
+        [field: SerializeField] public GameObject TargetObject { get; private set; }
+
+        public void OnValidate()
+        {
+            inspectorName = ArgType.ToString();
         }
     }
 }
