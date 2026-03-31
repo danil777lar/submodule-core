cec8168e0a222d052ad8f416929b2c1ada40078f
Danil Kolbasenko <danil777lar@gmail.com>
2026-03-30 22:44:08 -0300

    add placeholder ads service

---
+++ Scripts/Core/Services/Ads/PlaceholderAdsService.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+namespace Larje.Core.Services
+{
+    [BindService(typeof(IAdsService))]
+    public class PlaceholderAdsService : Service, IAdsService
+    {
+        public bool Initialized => true;
+
+        public bool InterstitialAdAvailable => false;
+
+        public bool RewardedAdAvailable => false;
+
+        public bool BannerShowing => false;
+
+        public float BannerHeight => 0f;
+
+        public event Action EventBannerShown;
+        public event Action EventBannerHidden;
+
+        public override void Init()
+        {
+        }
+
+        public bool ShowAppOpenAd()
+        {
+            return true;
+        }
+
+        public void ShowInterstitial(int interIndex = 0)
+        {
+        }
+
+        public void ShowRewarded(Action onAdShowStart, Action onAdShowClick, Action onAdShowComplete, Action onAdShowFailed)
+        {
+            onAdShowFailed?.Invoke();
+        }
+    }
+}
